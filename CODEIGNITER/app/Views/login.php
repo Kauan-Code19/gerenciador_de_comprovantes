93474a73ccf549d245dbeb78bea4515e645728d6
Kauan-Code19 <pkauprofissional@gmail.com>
2023-07-26 20:09:12 -0300

    Estilização

--- CODEIGNITER/app/Views/login.php
+++ CODEIGNITER/app/Views/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../public/Assets/CSS/index.css">
     <title>Tela de Login</title>
 </head>
 <body>
