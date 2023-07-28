168d19b90964e6852c13c24c4b2b497e197a4d6c
Kauan-Code19 <pkauprofissional@gmail.com>
2023-07-28 11:44:02 -0300

    EstlizacaoPageUsuario

--- app/Views/usuario/index.php
+++ app/Views/usuario/index.php
@@ -1,23 +1,31 @@
-<!-- <h2><?= esc($title) ?></h2> -->
-
-<table class="table">
-    <th>
-        <td>nome</td>
-        <td>email</td>
-    </th>
 <?php if (! empty($usuario) && is_array($usuario)): ?>
-
-    <?php foreach ($usuario as $usuario_item): ?>
-    <tr>
-        <td><?= esc($usuario_item['nome']) ?></td>
-        <td><?= esc($usuario_item['email']) ?></td>
-        <!-- <p><a href="/usuario/<?= esc($usuario_item['id'], 'url') ?>">View article</a></p> -->
-    </tr>
-    <?php endforeach ?>
-
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($usuario as $usuario_item): ?>
+                <tr>
+                    <td><?= esc($usuario_item['id']) ?></td>
+                    <td><?= esc($usuario_item['nome']) ?></td>
+                    <td><?= esc($usuario_item['email']) ?></td>
+                    <td><div>
+                        <button>
+                            <p><a href="/usuario/edicao/<?= esc($usuario_item['id'], 'url') ?>">editar</a></p>
+                        </button>
+                        <button>
+                            <p><a href="/usuario/deletar/<?= esc($usuario_item['id'], 'url') ?>">deletar</a></p>
+                        </button>
+                    </div></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
 <?php else: ?>
-
     <h3>Sem usuários cadastrados</h3>
 <?php endif ?>
-
-</table>
