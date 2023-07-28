c4cd957c8abc7daf5a5954ad64f13c41ea4ced39
Kauan-Code19 <pkauprofissional@gmail.com>
2023-07-28 10:37:26 -0300

    Base para estilização Usuario

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,7 +2,10 @@
 <html>
 <head>
     <title>CodeIgniter Tutorial</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/style/title.css">
 </head>
-<body>
-
-    <h1><?= esc($title) ?></h1>
+<body class="fundo">
+    <div class="text-center">
+        <h1><?= esc($title) ?></h1>
+    </div>
--- app/Views/usuario/index.php
+++ app/Views/usuario/index.php
@@ -1,22 +1,26 @@
-<h2><?= esc($title) ?></h2>
-
 <?php if (! empty($usuario) && is_array($usuario)): ?>
-
-    <?php foreach ($usuario as $usuario_item): ?>
-
-        <h3><?= esc($usuario_item['nome']) ?></h3>
-
-        <div class="main">
-            <?= esc($usuario_item['email']) ?>
-        </div>
-        <p><a href="/usuario/<?= esc($usuario_item['id'], 'url') ?>">View article</a></p>
-
-    <?php endforeach ?>
-
+    <table class="table table-hover table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th scope="col" class="bg-combined text-white text-center">ID</th>
+                <th scope="col" class="bg-name text-white text-center">Nome</th>
+                <th scope="col" class="bg-email text-white text-center">E-mail</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($usuario as $usuario_item): ?>
+                <tr>
+                    <td class="text-center text-white"><span class="badge badge-custom"><?= esc($usuario_item['id']) ?></td>
+                    <td class="bgb-name text-white text-center"><?= esc($usuario_item['nome']) ?></td>
+                    <td class="bgb-email text-white text-center"><?= esc($usuario_item['email']) ?></td>
+                </tr>
+            <?php endforeach ?>    
+        </tbody>
+    </table>
 <?php else: ?>
-
+            
     <h3>No News</h3>
-
+            
     <p>Unable to find any news for you.</p>
-
+            
 <?php endif ?>
