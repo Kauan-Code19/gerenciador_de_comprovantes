7e76e284f1b12528ec82f29e050b3c85ec3db05b
Kauan-Code19 <pkauprofissional@gmail.com>
2023-07-28 14:28:18 -0300

    Painel de Depuracao em footer
    div id toolbarContainer

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <div class="text-center">
-        <h1><?= esc($title) ?></h1>
-    </div>
+    <div class="container">
+        <div class="text-center">
+            <h1><?= esc($title) ?></h1>
+        </div>
--- app/Views/usuario/index.php
+++ app/Views/usuario/index.php
@@ -1,31 +1,38 @@
-<?php if (! empty($usuario) && is_array($usuario)): ?>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Ação</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($usuario as $usuario_item): ?>
-                <tr>
-                    <td><?= esc($usuario_item['id']) ?></td>
-                    <td><?= esc($usuario_item['nome']) ?></td>
-                    <td><?= esc($usuario_item['email']) ?></td>
-                    <td><div>
-                        <button>
-                            <p><a href="/usuario/edicao/<?= esc($usuario_item['id'], 'url') ?>">editar</a></p>
-                        </button>
-                        <button>
-                            <p><a href="/usuario/deletar/<?= esc($usuario_item['id'], 'url') ?>">deletar</a></p>
-                        </button>
-                    </div></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-<?php else: ?>
-    <h3>Sem usuários cadastrados</h3>
-<?php endif ?>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <?php if (! empty($usuario) && is_array($usuario)): ?>
+                    <div class="table-container" style="max-height: 400px; overflow-y: auto;">
+                        <table class="table">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Ação</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($usuario as $usuario_item): ?>
+                                    <tr class="text-center">
+                                        <td><?= esc($usuario_item['id']) ?></td>
+                                        <td><?= esc($usuario_item['nome']) ?></td>
+                                        <td><?= esc($usuario_item['email']) ?></td>
+                                        <td><div>
+                                            <button>
+                                                <p><a href="/usuario/edicao/<?= esc($usuario_item['id'], 'url') ?>">editar</a></p>
+                                            </button>
+                                            <button>
+                                                <p><a href="/usuario/deletar/<?= esc($usuario_item['id'], 'url') ?>">deletar</a></p>
+                                            </button>
+                                        </div></td>
+                                    </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php else: ?>
+                    <h3>Sem usuários cadastrados</h3>
+                <?php endif ?>
+            </div>
+        </div>
+    </div>
