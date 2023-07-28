8bc6aac90c55cd33a19bc2356cedc741389b3d14
Kauan-Code19 <pkauprofissional@gmail.com>
2023-07-28 17:40:21 -0300

    Organizando CSS de Usuario
    adicionado usuario.css

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -3,6 +3,7 @@
 <head>
     <title>CodeIgniter Tutorial</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/style/usuario.css">
 </head>
 <body>
     <div class="container">
--- app/Views/usuario/index.php
+++ app/Views/usuario/index.php
@@ -1,10 +1,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <?php if (! empty($usuario) && is_array($usuario)): ?>
-                    <div class="table-container" style="max-height: 400px; overflow-y: auto;">
+                    <div class="table-container">
                         <table class="table">
                             <thead>
-                                <tr class="text-center">
+                                <tr class="text-center th">
                                     <th>ID</th>
                                     <th>Nome</th>
                                     <th>Email</th>
@@ -13,7 +13,7 @@
                             </thead>
                             <tbody>
                                 <?php foreach ($usuario as $usuario_item): ?>
-                                    <tr class="text-center">
+                                    <tr class="text-center tb">
                                         <td><?= esc($usuario_item['id']) ?></td>
                                         <td><?= esc($usuario_item['nome']) ?></td>
                                         <td><?= esc($usuario_item['email']) ?></td>
@@ -22,7 +22,7 @@
                                                 <p><a href="/usuario/edicao/<?= esc($usuario_item['id'], 'url') ?>">editar</a></p>
                                             </button>
                                             <button>
-                                                <p><a href="/usuario/deletar/<?= esc($usuario_item['id'], 'url') ?>">deletar</a></p>
+                                                <p><a href="/usuario/deletar/<?= esc($usuario_item['id'], 'url') ?>" class="link-danger">deletar</a></p>
                                             </button>
                                         </div></td>
                                     </tr>
