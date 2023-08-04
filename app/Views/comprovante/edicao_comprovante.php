f10a8e4fd792bbb047d0863a6f6b9691f3634727
Kauan-Code19 <pkauprofissional@gmail.com>
2023-08-04 16:20:47 -0300

    Criacao page edicao_comprovantes e mudancas na index

--- app/Views/comprovante/edicao_comprovante.php
+++ app/Views/comprovante/edicao_comprovante.php
@@ -2,35 +2,35 @@
             <div class="row justify-content-center">
                 <div class="card col-md-8">
                     <div class="card-body card-color">
-                        <form action="/Comprovante/salvar" method="post" class="divFormComprovante" enctype="multipart/form-data">
+                        <form action="/Comprovante/atualizar" method="post" class="divFormComprovante" enctype="multipart/form-data">
                             <div class="row mb-3">
                                 <div class="col-md-6">
                                     <label for="descricao">Descrição:</label>
-                                    <input type="text" class="form-control btn-upload" id="descricao" name="descricao" required>
+                                    <input type="text" class="form-control btn-upload" id="descricao" name="descricao" value="" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="numero">Número:</label>
-                                    <input type="text" class="form-control btn-upload" id="numero" name="numero" required>
+                                    <input type="text" class="form-control btn-upload" id="numero" name="numero" value="" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-md-6">
                                     <label for="valor">Valor:</label>
-                                    <input type="number" class="form-control btn-upload" id="valor" name="valor" step="0.01" required>
+                                    <input type="number" class="form-control btn-upload" id="valor" name="valor" step="0.01" value="" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="dataPagamento">Data de Pagamento:</label>
-                                    <input type="date" class="form-control btn-upload" id="dataPagamento" name="dataPagamento" required>
+                                    <input type="date" class="form-control btn-upload" id="dataPagamento" name="dataPagamento" value="" required>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <div class="col-md-6">
                                     <label for="dataVencimento">Data de Vencimento:</label>
-                                    <input type="date" class="form-control btn-upload" id="dataVencimento" name="dataVencimento" required>
+                                    <input type="date" class="form-control btn-upload" id="dataVencimento" name="dataVencimento" value="" required>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="metodo">Método de Pagamento:</label>
-                                    <select class="form-control btn-upload" id="metodo" name="metodo" required>
+                                    <select class="form-control btn-upload" id="metodo" name="metodo" value="" required>
                                         <option value="pix">PIX</option>
                                         <option value="boleto">Boleto</option>
                                         <option value="cartao">Cartão</option>
@@ -40,12 +40,12 @@
                             <div class="row mb-3">
                                 <div class="col-md-12">
                                     <label for="comprovante">Upload do Comprovante:</label>
-                                    <input type="file" class="form-control-file btn-upload" id="comprovante" name="comprovante" required>
+                                    <input type="file" class="form-control-file btn-upload" id="comprovante" name="comprovante" value="" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
-                                    <button type="submit" class="btn btn-aplicacao">Salvar</button>
+                                    <button type="submit" class="btn btn-aplicacao">Atualizar</button>
                                 </div>
                             </div>
                         </form>
