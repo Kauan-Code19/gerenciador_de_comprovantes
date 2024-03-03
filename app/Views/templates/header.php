2a65a11143c75dba35b51afcd34f69e3631ba549
Luis Filipe <luisphilip90@gmail.com>
2024-03-03 10:15:39 -0300

    adding favicon

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= base_url(); ?>assets/style/style.css">
+    <link rel="shortcut icon" type="image/png" href="<?= base_url(); ?>/favicon.ico">
 </head>
 <body>
     <div class="divMae">
--- app/Views/templates/headerlogin.php
+++ app/Views/templates/headerlogin.php
@@ -4,6 +4,7 @@
     <title><?= esc($title) ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url(); ?>assets/style/style.css">
+    <link rel="shortcut icon" type="image/png" href="<?= base_url(); ?>/favicon.ico">
 </head>
 <body>
     <div class="divMae d-flex flex-column justify-content-center w-100 h-100">
